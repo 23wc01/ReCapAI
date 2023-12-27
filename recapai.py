6b7c63c8da7f4cb03abd40615c12bb533e3a3473
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2023-12-26 21:16:42 -0800

    modify - delete audio-chunking code for now bc AssemblyAI has GENEROUS 5hr audio/month limit, unlike WhisperAI. Modified OpenAI chat completion for compatibility with its latest API version

--- recapai.py
+++ recapai.py
@@ -1,75 +1,77 @@
 import openai
+from openai import OpenAI
 from docx import Document
-from pydub import AudioSegment
+import assemblyai as aai
 import os
 import gradio
 import shutil
 
-#In cmd, set up API key with: setx "myAPIKey"
+#In cmd, set up each API key with command: setx "myAPIKey"
 client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+aai.settings.api_key = os.getenv('ASSEMBLYAI_KEY')
 
 # Transcribe audio into text
 def transcribe(audio_file_path):
-    with open(audio_file_path, 'rb') as audio_file:
-        transcription = openai.Audio.transcribe('whisper-1', audio_file)
-    return transcription['text']
+    transcriber = aai.Transcriber()
+    transcript = transcriber.transcribe(audio_file_path)
+    return transcript
 
 # Returns a dictionary of the summaries
-def recap(transcription):
-    summary = summarizer(transcription)
-    sentiment = sentiment_analyzer(transcription)
-    key_points = key_points_extracter(transcription)
-    action_items = tasks_extracter(transcription)
+def recap(transcript):
+    summary = summarizer(transcript)
+    sentiment = sentiment_analyzer(transcript)
+    key_points = key_points_extracter(transcript)
+    tasks = tasks_extracter(transcript)
     return {
         "sentiment": sentiment,
         "summary": summary,
         "key_points": key_points,
-        "action_items": action_items
+        "tasks": tasks
     }
     
-#Analyze tone & emotion
-def sentiment_analyzer(transcription):
-    completion = openai.ChatCompletion.create(
+# Analyze tone & emotion
+def sentiment_analyzer(transcript):
+    completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages = [
             {"role": "system", "content": "You are an AI experienced in language and emotion analysis, analyze the sentiment of the following text. Consider overall tone of the discussion, emotion conveyed by language used, and the context in which words and phrases are used. Indicate if sentiment is positvive, negative, or neutral, and provide brief explanations for your analysis where possible."},
-            {"role": "user", "content": transcription}
+            {"role": "user", "content": transcript}
         ]
     )
-    return completion['choices'][0]['message']['content']
+    return completion.choices[0].message.content
 
 # Summarize into 1 abstract paragraph
-def summarizer(transcription):
-    completion = openai.ChatCompletion.create(
+def summarizer(transcript):
+    completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages = [
             {"role": "system", "content": "You are a highly skilled AI trained in language comprehension and sumarization. Read the following text and summarize it into a concise abstract paragraph. Aim to retain the most important points, give a coherent and readable summary that could help a person understand the main points of the disscussion without needing to read the entire text. Avoid unnecessary details or tangential points."},
-            {"role": "user", "content": transcription}
+            {"role": "user", "content": transcript}
         ]
     )
-    return completion['choices'][0]['message']['content']
+    return completion.choices[0].message.content
 
 # List key points
-def key_points_extracter(transcription):
-    completion = openai.ChatCompletion.create(
+def key_points_extracter(transcript):
+    completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages = [
             {"role": "system", "content": "You are a proficient AI that distills information into key points. Based on the following text, identify and list the main points that were discussed. These should be the most important ideas, finidngs, or topics that are crucial to the essence of the discussion. Your goal is to provide a list someone could scan over quickly and understand what was talked about."},
-            {"role": "user", "content": transcription}
+            {"role": "user", "content": transcript}
         ]
     )
-    return completion['choices'][0]['message']['content']
+    return completion.choices[0].message.content
 
 # List action items (assignments, tasks, actions, etc.)
-def tasks_extracter(transcription):
-    completion = openai.ChatCompletion.create(
+def tasks_extracter(transcript):
+    completion = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages = [
             {"role": "system", "content": "You are an AI expert in analyzing conversations and extracting action items. Please review text and identify any tasks, assignments, or actions that were agreed upon or mentioned as needing to be done. These could be tasks assigned to specific individuals, or general actions that the group has decided to take. List action items clearly and concisely."},
-            {"role": "user", "content": transcription}
+            {"role": "user", "content": transcript}
         ]
     )
-    return completion['choices'][0]['message']['content']
+    return completion.choices[0].message.content
 
 # Save summaries into a .docx document
 def save_as_docx(recap, filename):
@@ -91,36 +93,21 @@ def printRecap(recap):
 #Run functions
 audio_file_path = "./"
 audio_file_name = "EarningsCall.wav"
+transcript = transcribe(audio_file_path+audio_file_name)
 
-#Split Audio so that it's under max limit of 25 MG
-audio = AudioSegment.from_mp3(audio_file_path + audio_file_name)
+recapped_info = recap(transcript.text)
 
-# Total video = 175,000 milliseconds
-first_half_milliseconds = 175000/2
-part1 = audio[:first_half_milliseconds]
-part2 = audio[first_half_milliseconds:]
-audio_file_path_part1 = audio_file_path + "Part1" + audio_file_name
-audio_file_path_part2 = audio_file_path + "Part2" + audio_file_name
+printRecap(recapped_info)
+save_as_docx(recapped_info, "./reacpai.docx")
 
-part1.export(audio_file_path_part1)
-part2.export(audio_file_path_part2)
-
-transcription1 = transcribe(audio_file_path_part1)
-transcription2 = transcribe(audio_file_path_part2)
-transcription = transcription1 + transcription2
-recap_summary = recap(transcription)
-
-printRecap(recap_summary)
-save_as_docx(recap_summary, "./reacpai.docx")
-
-def process_file(audio_filepath):
+"""def process_file(audio_filepath):
     path = "./" + os.path.basename(audio_filepath)  
     shutil.copyfile(audio_filepath.name, path)
     return recap(path)
 
 getAudioFile = gradio.File(file_count="single", file_types=["audio"])
 UI = gradio.Interface(process_file, getAudioFile, "file")
-UI.launch()
+UI.launch()"""
 
     
 
