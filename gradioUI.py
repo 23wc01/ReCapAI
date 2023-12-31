ec9003f5dd249866de7537a58ed97723a59888ed
23wc01 <132469274+23wc01@users.noreply.github.com>
2023-12-30 19:00:35 -0800

    Modify - gradioUI.py to include documentations & comments for functions

--- gradioUI.py
+++ gradioUI.py
@@ -10,11 +10,22 @@ from transcribe import aai_transcribe
 # In cmd, quick setup of API key by entering: setx KEY_NAME "my_key"
 ASSEMBLYAI_KEY = os.getenv('ASSEMBLYAI_KEY')
 
-def UI_recapai(audio_file, output_types, file_name="recapai"):
-    print(audio_file.name)
+def UI_recapai(audio_file="./upload_audio/EarningsCall.wav", output_types=["pdf", "docx", "txt"], file_name="recapai"):
+    """Runs recapai program on gradio online & sharable interface! Can be seperated into 3 main steps: Transcribe audio, AI recap transcript, save completion (recap) to desired file type(s)
+        Parmeter: 
+        audio_file -- str, Specifies path to file (default= ./upload_audio/EarningsCall.wav) 
+        output_types -- list, Specifies which type(s) of file recap should be saved as. Can save as multiple file types. (default = pdf, docx, AND txt)
+        recap_file_path -- str, Specifies path ONLY of outputted recap file (default = ./output_recap/)
+        recap_file_name -- str, Specifies name of outputted recap file (default = EarningsCallRecap)
+    Output:
+        outputs -- list[str], file addresses of saved files (for gradio to display on UI)
+    """
+    # Transcribe audio
     transcript = aai_transcribe(ASSEMBLYAI_KEY, audio_file.name)
     os.remove(audio_file.name) # Remove audio file after transcription
+    # AI recap the transcript
     recap_info = recapai.recap(transcript)
+    # Save completion (recap) to desired file type(s), add file names to outputs so gradio can recieve them
     outputs = []
     for output_type in output_types:
         if output_type == "pdf":
@@ -26,6 +37,10 @@ def UI_recapai(audio_file, output_types, file_name="recapai"):
     return outputs
 
 def get_gradio_UI():
+    """Setup gradio UI object so that user inputs can be passed to UI_recapai for running the main components of the program
+        Output:
+            UI -- (gradio UI object) passes it to main so that it can be .launch() on web browser 
+    """
     UI = gradio.Interface(UI_recapai,
     inputs=[
             gradio.File(label="Audio File"),
