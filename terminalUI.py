48bd4fc023195ed005f96ae2feb521763a031a4b
23wc01 <132469274+23wc01@users.noreply.github.com>
2023-12-30 19:04:57 -0800

    Modify - terminalUI.py

--- terminalUI.py
+++ terminalUI.py
@@ -10,7 +10,7 @@ from transcribe import aai_transcribe
 ASSEMBLYAI_KEY = os.getenv('ASSEMBLYAI_KEY')
 
 def terminal_recapai(audio_file="./upload_audio/EarningsCall.wav", output_types=["pdf","docx","txt"], recap_file_path="./output_recap/", recap_file_name="EarningsCallRecap"):
-    """Runs recapai program! Can be seperated into 3 main steps: transcribe audio, AI recap transcript, completion (recap) to desired file type(s)
+    """Runs recapai program! Can be seperated into 3 main steps: transcribe audio, AI recap transcript, save completion (recap) to desired file type(s)
         Parmeter: 
         audio_file -- Specifies path to file (default= ./upload_audio/EarningsCall.wav) 
         output_types -- Specifies which type(s) of file recap should be saved as. Can save as multiple file types. (default = pdf, docx, AND txt)
