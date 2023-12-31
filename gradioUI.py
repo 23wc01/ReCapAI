c28567bbd3c1b0592bcd2d13d63982fb310205a8
Wen-Hsin Chen <chenwenhsin0@gmail.com>
2023-12-30 21:09:32 -0800

    Modify - added documentation to UI files

--- gradioUI.py
+++ gradioUI.py
@@ -11,6 +11,15 @@ from transcribe import aai_transcribe
 ASSEMBLYAI_KEY = os.getenv('ASSEMBLYAI_KEY')
 
 def UI_recapai(audio_file, output_types, file_name="recapai"):
+    """Runs recapai program on gradio online & sharable interface! Can be seperated into 3 main steps: Transcribe audio, AI recap transcript, save completion (recap) to desired file type(s)
+        Parmeter: 
+        audio_file -- str, Specifies path to file (default= ./upload_audio/EarningsCall.wav) 
+        output_types -- list, Specifies which type(s) of file recap should be saved as. Can save as multiple file types. (default = pdf, docx, AND txt)
+        recap_file_path -- str, Specifies path ONLY of outputted recap file (default = ./output_recap/)
+        recap_file_name -- str, Specifies name of outputted recap file (default = EarningsCallRecap)
+    Output:
+        outputs -- list[str], file addresses of saved files (for gradio to display on UI)
+    """
     print(audio_file.name)
     transcript = aai_transcribe(ASSEMBLYAI_KEY, audio_file.name)
     os.remove(audio_file.name) # Remove audio file after transcription
@@ -26,6 +35,10 @@ def UI_recapai(audio_file, output_types, file_name="recapai"):
     return outputs
 
 def get_gradio_UI():
+    """Setup gradio UI object so that user inputs can be passed to UI_recapai for running the main components of the program
+        Output:
+            UI -- (gradio UI object) passes it to main so that it can be .launch() on web browser 
+    """
     UI = gradio.Interface(UI_recapai,
     inputs=[
             gradio.File(label="Audio File"),
